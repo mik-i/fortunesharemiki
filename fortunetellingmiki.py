ec606222348dbeed6ce58db7b9c119dbce3a621d
fukkinwaritai <138385189+fukkinwaritai@users.noreply.github.com>
2023-11-17 23:00:03 +0900

    メッセージ変更および追加
    変更内容１：出力メッセージを変更してみました。（日本語追記）
    変更内容２：最終行に「占い終了」のメッセージ追記してみました。

--- fortunetellingmiki.py
+++ fortunetellingmiki.py
@@ -3,10 +3,11 @@ import random
 n = random.randint(0,2)
 
 if n == 0:
-  unsei = "daikichi"
+  unsei = "daikichi_大吉"
 elif n == 1:
-  unsei = "chukichi"
+  unsei = "chukichi_中吉"
 else:
-  unsei = "shokichi"
+  unsei = "shokichi_小吉"
 
 print(unsei)
+print(uranaisyuryo_占い終了)
